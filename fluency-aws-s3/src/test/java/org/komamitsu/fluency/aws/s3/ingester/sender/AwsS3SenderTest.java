ffae1f8299845983362db11531e7f8d5287509e2
Mitsunori Komatsu <komamitsu@gmail.com>
2019-04-09 23:51:10 +0900

    Add AwsS3SenderTest#send

--- fluency-aws-s3/src/test/java/org/komamitsu/fluency/aws/s3/ingester/sender/AwsS3SenderTest.java
+++ fluency-aws-s3/src/test/java/org/komamitsu/fluency/aws/s3/ingester/sender/AwsS3SenderTest.java
@@ -16,14 +16,28 @@
 
 package org.komamitsu.fluency.aws.s3.ingester.sender;
 
+import com.google.common.io.ByteStreams;
+import com.google.common.primitives.Bytes;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.GZIPInputStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -40,7 +54,7 @@ class AwsS3SenderTest
     void buildClientWithDefaults()
     {
         AwsS3Sender.Config config = new AwsS3Sender.Config();
-        config.setKeySuffix("foo/bar");
+        config.setKeySuffix(".data");
 
         S3Client s3Client = mock(S3Client.class);
         S3ClientBuilder s3ClientBuilder = mock(S3ClientBuilder.class);
@@ -58,7 +72,7 @@ class AwsS3SenderTest
     void buildClientWithCustomizedConfig()
     {
         AwsS3Sender.Config config = new AwsS3Sender.Config();
-        config.setKeySuffix("foo/bar");
+        config.setKeySuffix(".data");
         config.setEndpoint("https://another.s3endpoi.nt");
         config.setRegion("ap-northeast-1");
         config.setAwsAccessKeyId("foo");
@@ -81,5 +95,55 @@ class AwsS3SenderTest
         verify(s3ClientBuilder, times(1)).endpointOverride(eq(URI.create("https://another.s3endpoi.nt")));
         verify(s3ClientBuilder, times(1)).region(eq(Region.AP_NORTHEAST_1));
         verify(s3ClientBuilder, times(1)).credentialsProvider(any());
+    }
+
+    @Test
+    void send()
+            throws IOException
+    {
+        AwsS3Sender.Config config = new AwsS3Sender.Config();
+        config.setKeySuffix(".data");
+
+        S3Client s3Client = mock(S3Client.class);
+        S3ClientBuilder s3ClientBuilder = mock(S3ClientBuilder.class);
+        doReturn(s3Client).when(s3ClientBuilder).build();
+        doAnswer(invocation -> {
+            PutObjectRequest request = invocation.getArgument(0);
+
+            assertEquals("hello.world", request.bucket());
+
+            Pattern pattern = Pattern.compile("(\\d{4})/(\\d{2})/(\\d{2})/(\\d{2})/(\\d{2})-(\\d{2})-(\\d{5})");
+            Matcher matcher = pattern.matcher(request.key());
+            assertTrue(matcher.find());
+            assertEquals(7, matcher.groupCount());
+            int year = Integer.valueOf(matcher.group(1));
+            int month = Integer.valueOf(matcher.group(2));
+            int day = Integer.valueOf(matcher.group(3));
+            int hour = Integer.valueOf(matcher.group(4));
+            int minute = Integer.valueOf(matcher.group(5));
+            int second = Integer.valueOf(matcher.group(6));
+            int nanoSeconds = Integer.valueOf(matcher.group(7));
+            Instant timestampFromkey =
+                    ZonedDateTime.of(year, month, day, hour, minute, second, nanoSeconds, ZoneOffset.UTC)
+                            .toInstant();
+
+            assertTrue(Instant.now().isAfter(timestampFromkey));
+            assertTrue(Instant.now().minusSeconds(5).isBefore(timestampFromkey));
+
+            RequestBody body = invocation.getArgument(1);
+            try (InputStream in = new GZIPInputStream(body.contentStreamProvider().newStream())) {
+                byte[] content = ByteStreams.toByteArray(in);
+                assertEquals("0123456789", new String(content, StandardCharsets.UTF_8));
+            }
+
+            return null;
+        }).when(s3Client).putObject(any(PutObjectRequest.class), any(RequestBody.class));
+
+        AwsS3Sender sender = new AwsS3Sender(s3ClientBuilder, config);
+
+        sender.send("hello.world",
+                ByteBuffer.wrap("0123456789".getBytes(StandardCharsets.UTF_8)));
+
+        sender.close();
     }
 }
