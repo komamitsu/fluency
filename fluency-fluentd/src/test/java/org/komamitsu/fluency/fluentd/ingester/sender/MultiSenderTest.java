4607e02921711e46929f66f3106acf273da4352e
Mitsunori Komatsu <komamitsu@gmail.com>
2020-11-11 23:56:38 +0900

    Tweak expected values in MultiSenderTest

--- fluency-fluentd/src/test/java/org/komamitsu/fluency/fluentd/ingester/sender/MultiSenderTest.java
+++ fluency-fluentd/src/test/java/org/komamitsu/fluency/fluentd/ingester/sender/MultiSenderTest.java
@@ -293,8 +293,8 @@ class MultiSenderTest
 
         assertEquals(2, connectCount);
         // This test doesn't use actual PackedForward format so that it can simply test MultiSender itself.
-        // But w/o ack responses, Sender can't detect dropped requests. So some margin for expected result is allowed here
-        long minExpectedRecvLen = ((long) (concurency * (sslEnabled ? 0.6 : 0.8)) * reqNum) * 10;
+        // But w/o ack responses, Sender can't detect dropped requests. So some margin for expected result is allowed here.
+        long minExpectedRecvLen = ((long) (concurency * (sslEnabled ? 0.5 : 0.8)) * reqNum) * 10;
         long maxExpectedRecvLen = ((long) concurency * reqNum) * 10;
         assertThat(recvLen, is(greaterThanOrEqualTo(minExpectedRecvLen)));
         assertThat(recvLen, is(lessThanOrEqualTo(maxExpectedRecvLen)));
