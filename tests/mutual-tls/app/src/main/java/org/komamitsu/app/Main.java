31c855076abf3a2551540095817fb623b2d857b2
Mitsunori Komatsu <komamitsu@gmail.com>
2021-05-17 00:41:43 +0900

    Add tests/mutual-tls

---
+++ tests/mutual-tls/app/src/main/java/org/komamitsu/app/Main.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2021 Mitsunori Komatsu (komamitsu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.komamitsu.app;
+
+import org.komamitsu.fluency.Fluency;
+import org.komamitsu.fluency.fluentd.FluencyBuilderForFluentd;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        String tag = args[2];
+
+        FluencyBuilderForFluentd builder = new FluencyBuilderForFluentd();
+        builder.setSslEnabled(true);
+        builder.setWaitUntilBufferFlushed(5000);
+        builder.setWaitUntilFlusherTerminated(5000);
+        builder.setSenderMaxRetryCount(4);
+        Fluency fluency = builder.build(host, port);
+
+        Map<String, Object> event = new HashMap<>();
+        event.put("name", "komamitsu");
+        event.put("comment", "zzz");
+        fluency.emit(tag, event);
+        fluency.close();
+    }
+}
