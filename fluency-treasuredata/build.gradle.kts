298e23ae8f0a48e68a8444b747ced7af08b14c38
yuokada <yuokada@users.noreply.github.com>
2024-07-12 01:55:15 +0900

    Add guava as a dependency library

--- fluency-treasuredata/build.gradle.kts
+++ fluency-treasuredata/build.gradle.kts
@@ -2,5 +2,6 @@ dependencies {
   implementation(projects.fluencyCore)
 
   implementation("com.treasuredata.client:td-client:1.1.1")
+  implementation("com.google.guava:guava:33.2.1-jre")
   implementation("com.squareup.okhttp3:okhttp:3.14.9")
 }
