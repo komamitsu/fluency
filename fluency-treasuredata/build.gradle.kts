8a84d7e8fc54c779f254d0a436807946d16a7728
yuokada <yuokada@users.noreply.github.com>
2024-07-12 09:38:19 +0900

    Add dependency libraries

--- fluency-treasuredata/build.gradle.kts
+++ fluency-treasuredata/build.gradle.kts
@@ -2,6 +2,8 @@ dependencies {
   implementation(projects.fluencyCore)
 
   implementation("com.treasuredata.client:td-client:1.1.1")
-  implementation("com.google.guava:guava:33.2.1-jre")
+  implementation("com.google.guava:guava:33.1.0-jre")
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-json-org:2.16.1")
+  testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:2.16.1")
   implementation("com.squareup.okhttp3:okhttp:3.14.9")
 }
